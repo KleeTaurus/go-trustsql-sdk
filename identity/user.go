791dcf4fd38dc2d80e00455c11177c0f8a76eedd
dalei <guoyunlei@live.com>
2018-03-20 15:24:44 +0800

    Api for identity for trustsql
    ref #6

---
+++ identity/user.go
@@ -0,0 +1,21 @@
+package identity
+
+// UserRegister 注册用户
+func UserRegister() {
+}
+
+// GetUserInfo 获取用户信息
+func GetUserInfo() {
+}
+
+// AccountRegister 创建用户账户
+func AccountRegister() {
+}
+
+// GetAccounts 获取用户的账户地址列表
+func GetAccounts() {
+}
+
+// GetPubkeyOfAccount 获取用户的账户公钥
+func GetPubkeyOfAccount() {
+}
