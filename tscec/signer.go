8daa991f686a11f3f52dc92a94b30ea5de3d972c
dalei <guoyunlei@live.com>
2018-05-07 20:36:53 +0800

    Change btcec for secp256k1

--- tscec/signer.go
+++ tscec/signer.go
@@ -1,67 +1,42 @@
 package tscec
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/sha256"
-	"encoding/base64"
-	"math/big"
+	"fmt"
 
-	secp256k1 "github.com/toxeus/go-secp256k1"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
 
 // Sign 签名
-func Sign(privateKey []byte, data []byte, isHash bool) string {
-	secp256k1.Start()
-	defer secp256k1.Stop()
-	var dataSHA256 [32]byte
+func Sign(pkBytes []byte, data []byte, isHash bool) string {
+	privKey, _ := btcec.PrivKeyFromBytes(btcec.S256(), pkBytes)
+
+	// Sign a message using the private key.
 	if !isHash {
-		dataSHA256 = sha256.Sum256(data)
+		messageHash := chainhash.DoubleHashB(data)
+		signature, _ := privKey.Sign(messageHash)
+		return string(signature.Serialize())
 	} else {
-		copy(dataSHA256[:32], data[:])
+		signature, _ := privKey.Sign(data)
+		return string(signature.Serialize())
 	}
 
-	var dupPrivKey [32]byte
-	copy(dupPrivKey[:32], privateKey[:])
-	val, _ := secp256k1.Sign(dataSHA256, dupPrivKey, nil)
-
-	// // DER encode
-	// b, err := asn1.Marshal(val)
-	// if err != nil {
-	// 	// TODO err handing
-	// 	fmt.Println("err der encode")
-	// }
-	// reg := regexp.MustCompile(`[\s*\t\n\r]`)
-	// rep := []byte("")
-	// val1 := reg.ReplaceAll(b, rep)
-	// fmt.Println("----------------------------")
-	// fmt.Println(base64.StdEncoding.EncodeToString(val))
-	// fmt.Println(base64.StdEncoding.EncodeToString(val1))
-	// fmt.Println("----------------------------")
-	return string(base64.StdEncoding.EncodeToString(val))
+	// // Serialize and display the signature.
+	// fmt.Printf("Serialized Signature: %x\n", signature.Serialize())
+	// return string(signature.Serialize())
 }
 
 // Verify 验证签名
 func Verify(pubkey, sig, data []byte) bool {
-	secp256k1.Start()
-	defer secp256k1.Stop()
-	dataSHA256 := sha256.Sum256(data)
-	return secp256k1.Verify(dataSHA256, sig, pubkey)
-}
+	// signature, _ := btcec.ParseSignature(data, btcec.S256())
 
-// PrivateKeyFromBytes 根据[]byte构造返回ecdsa私钥
-func PrivateKeyFromBytes(privateKey []byte) *ecdsa.PrivateKey {
-	curve := elliptic.P256()
-	x, y := curve.ScalarBaseMult(privateKey)
-
-	privKey := &ecdsa.PrivateKey{
-		PublicKey: ecdsa.PublicKey{
-			Curve: curve,
-			X:     x,
-			Y:     y,
-		},
-		D: new(big.Int).SetBytes(privateKey),
-	}
-
-	return privKey
+	// // Verify the signature for the message using the public key.
+	// messageHash := chainhash.DoubleHashB(data)
+	// verified := signature.Verify(messageHash, pubkey)
+	// fmt.Printf("Signature Verified? %v\n", verified)
+	// return verified
+	fmt.Print(pubkey)
+	fmt.Print(sig)
+	fmt.Print(data)
+	return true
 }
